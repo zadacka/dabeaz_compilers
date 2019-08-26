b5a7b6f2bd0ddca9ad8dba1ab68168ad1afab1ef
Alexander Zawadzki <MacBookPro@Alexanders-MacBook-Pro.local>
2019-08-26 10:41:13 -0500

    :

---
+++ compilers/wabbit/check.py
@@ -0,0 +1,13 @@
+# check.py
+#
+# This file will have the type-checking/validation 
+# part of the compiler.  There are a number of things
+# that need to be managed to make this work. First,
+# you have to have some notion of "type" in your compiler.
+# Second, you need to manage environments/scoping in order
+# to handle the names of definitions (variables, functions, etc).
+#
+# A key to this part of the project is going to be test coverage.
+# As you add code, think about how to add unit tests.  
+
+
---
+++ week_overview.md
@@ -0,0 +1,3 @@
+# Day 1
+* look at metal.py
+* look at compilers/sillywabbit/ and work through material there
