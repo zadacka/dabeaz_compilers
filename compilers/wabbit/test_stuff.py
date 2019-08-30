65643329d81391b1c792707e46d0cf132b055092
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2019-08-30 15:53:31 -0500

    test_stuff.py: make test_stuff use Test/*.wb files as input

--- compilers/wabbit/test_stuff.py
+++ compilers/wabbit/test_stuff.py
@@ -1,51 +1,27 @@
 from time import sleep
 
-from compilers.programs import Constant, Variable, Float, Assignment, NamedLocation, BinaryOperator, Print, program1, \
-    Integer, If, While
 from compilers.wabbit.check import check_program
 from compilers.wabbit.ircode import generate_ircode
+from compilers.wabbit.parse import Parser
+from compilers.wabbit.tokenizer import tokenize
 from compilers.wabbit.wasm import WasmEncoder, i32, f64
 
-program = [
-    Constant('n', Integer(10)),
-    Variable('x', Integer(1), Integer.type),
-    Variable('fact', Integer(1), Integer.type),
-    While(
-        BinaryOperator('<', NamedLocation('x'), NamedLocation('n')),
-        [
-            Assignment(NamedLocation('fact'), BinaryOperator(
-                '*',
-                NamedLocation('fact'),
-                NamedLocation('x')
-            )),
-            Print(NamedLocation('fact')),
-            Assignment(NamedLocation('x'),
-                       BinaryOperator('+',
-                                      NamedLocation('x'),
-                                      Integer(1)
-                                      )
-                       )
-        ]
-    ),
-]
+with open('../Tests/inttest.wb') as f:
+    input_wabbit_code = f.read()
 
-check_program(program)  # side effect: fill in types (which we will need when generating IR code)
-ircode = generate_ircode(program)
-
-for line in ircode:
-    print(line)
+for token in tokenize(input_wabbit_code):
+    print(token)
+sleep(0.2)
+tokens = tokenize(input_wabbit_code)
+parsed_tokens = Parser(tokens).parse_statements()
+check_program(parsed_tokens)
+ircode = generate_ircode(parsed_tokens)
 
 encoder = WasmEncoder()
-
 # Declare the runtime function
 encoder.import_function("runtime", "_printi", [i32], [])
 encoder.import_function("runtime", "_printf", [f64], [])
-
-sleep(0.5)
-# Encode main(). Note: Return type changed to [].
 encoder.encode_function("main", [], [], [], ircode)
 with open('test_ir_out.wasm', 'wb') as file:
     file.write(encoder.encode_module())
 
-for line in encoder._wcode:
-    print(line)
